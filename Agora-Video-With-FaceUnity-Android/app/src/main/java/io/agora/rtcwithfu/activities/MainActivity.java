03b9eeebda03fbf4808fbb79616df89a107ed034
brianxcli <neteasy1210@gmail.com>
2020-08-25 17:18:02 +0800

    simplify main activity

--- Agora-Video-With-FaceUnity-Android/app/src/main/java/io/agora/rtcwithfu/activities/MainActivity.java
+++ Agora-Video-With-FaceUnity-Android/app/src/main/java/io/agora/rtcwithfu/activities/MainActivity.java
@@ -2,49 +2,69 @@ package io.agora.rtcwithfu.activities;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.io.File;
-import java.util.regex.Pattern;
 
 import io.agora.rtcwithfu.Constants;
 import io.agora.rtcwithfu.R;
 
 public class MainActivity extends Activity {
-    private static final String TAG = MainActivity.class.getName();
     private static final int REQUEST_CODE_ALL_PERMISSIONS = 999;
-
     private EditText mChannelName;
-    private OnCameraAndAudioPermissionListener mListener = new OnCameraAndAudioPermissionListener() {
-        @Override
-        public void onGrantResult(boolean granted) {
-
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-        mChannelName = (EditText) findViewById(R.id.edt_channel);
-        checkCameraPermission(this, mListener);
+        mChannelName = findViewById(R.id.edt_channel);
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (!permissionsGranted()) {
+            requestPermissions();
+        }
+    }
+
+    private boolean permissionsGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[] { Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO },
+                REQUEST_CODE_ALL_PERMISSIONS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        boolean granted = true;
+        if (requestCode == REQUEST_CODE_ALL_PERMISSIONS) {
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    granted = false;
+                    break;
+                }
+            }
+
+            if (!granted) {
+                requestPermissions();
+                Toast.makeText(this, getString(R.string.msg_permission_granted),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void onStartBroadcastClick(View view) {
@@ -56,103 +76,5 @@ public class MainActivity extends Activity {
             intent.putExtra(Constants.ACTION_KEY_ROOM_NAME, name);
             startActivity(intent);
         }
-    }
-
-    private boolean checkCameraPermission(Context context, OnCameraAndAudioPermissionListener listener) {
-        boolean granted = true;
-        boolean needToDoRealTest = isFlyme() || Build.VERSION.SDK_INT < Build.VERSION_CODES.M;
-
-        if (needToDoRealTest) {
-            Camera mCamera = null;
-            try {
-                mCamera = Camera.open();
-                Camera.Parameters mParameters = mCamera.getParameters();
-                mCamera.setParameters(mParameters);
-            } catch (Exception e) {
-                granted = false;
-                Log.i(TAG, Log.getStackTraceString(e));
-            }
-            if (mCamera != null) {
-                mCamera.release();
-            }
-
-            AudioRecord mAudioRecord = null;
-            try {
-                mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, 32000,
-                        AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
-                        AudioRecord.getMinBufferSize(32000, AudioFormat.CHANNEL_IN_MONO,
-                                AudioFormat.ENCODING_PCM_16BIT));
-                mAudioRecord.startRecording();
-            } catch (Exception e) {
-                granted = false;
-                Log.i(TAG, Log.getStackTraceString(e));
-            }
-
-            if (mAudioRecord != null) {
-                mAudioRecord.stop();
-                mAudioRecord.release();
-            }
-
-            File mFile = null;
-            try {
-                mFile = new File(Environment.getExternalStorageDirectory() + "/io.agora.rtcwithfu_test_per");
-                mFile.createNewFile();
-            } catch (Exception e) {
-                granted = false;
-            }
-            if (mFile != null) {
-                mFile.delete();
-            }
-        } else {
-            granted = !(ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
-                    ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
-                    ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED);
-        }
-
-        if (granted) {
-            if (listener != null) {
-                listener.onGrantResult(true);
-            }
-        } else if (context instanceof Activity) {
-            ActivityCompat.requestPermissions((Activity) context,
-                    new String[]{Manifest.permission.CAMERA,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.RECORD_AUDIO},
-                    REQUEST_CODE_ALL_PERMISSIONS);
-        }
-
-        return granted;
-    }
-
-    private static boolean isFlyme() {
-        if (Build.FINGERPRINT.contains("Flyme")
-                || Pattern.compile("Flyme", Pattern.CASE_INSENSITIVE).matcher(Build.DISPLAY).find()
-                || Build.MANUFACTURER.contains("Meizu")
-                || Build.BRAND.contains("MeiZu")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for (int result : grantResults) {
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, getString(R.string.msg_permission_rejected),
-                        Toast.LENGTH_SHORT).show();
-                return;
-            }
-        }
-
-        Toast.makeText(this, getString(R.string.msg_permission_granted),
-                Toast.LENGTH_SHORT).show();
-    }
-
-
-    public interface OnCameraAndAudioPermissionListener {
-        void onGrantResult(boolean granted);
     }
 }
